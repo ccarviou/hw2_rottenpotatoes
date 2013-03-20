933de502c67ab9bc56ff7c1f1c440bbc07332e66
ccarviou <saasbook@saasbook.(none)>
2013-03-20 08:51:41 -0700

    Change 1 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,38 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+	id = params[:id] # retrieve movie ID from URI route
+   	@movie = Movie.find(id) # look up movie by unique ID
+    	# will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+    
+
+
+	if params[:sortid] #save the sort request
+		session[:sortid] = params[:sortid]
+	else if session[:sortid] #if params has no sortid, recall old sortid request from session
+		change = true
+		params[:sortid] = session[:sortid]
+	end
+	end
+
+	@sortid = session[:sortid] #save the sort request as a accessible local variable
+
+	if change
+		redirect_to movies_path({:sortid=>@sortid}) #recall old sortid if member comes back from other page
+	else if
+		details = {'title'=>'title', 'release_date'=>'release_date'} #create a hash to compare and see if sort request is valid
+		if details.has_key?(@sortid) #determine if sort is valid
+			mov = Movie.order(details[@sortid]) #sort movies by valid sort type
+		else
+			@sortid = nil  #sort type was not valid and will not be stored
+			mov = Movie  #keep old order
+		end
+	end
+	end
+		@movies = mov.all #populate the movies table with all movies referenced by mov
   end
 
   def new
